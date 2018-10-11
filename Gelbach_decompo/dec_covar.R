b116516ae92d6a5ed8d5808f6dae7f3e36c6f6a5
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2018-10-11 11:15:29 -0700

    Add new argument conf.int, and plot-gamma_beta function

--- Gelbach_decompo/dec_covar.R
+++ Gelbach_decompo/dec_covar.R
@@ -60,7 +60,7 @@ update.ivreg <- function (object, formula., ..., evaluate = TRUE)
 
 
 dec_covar <- function(object, var_main, format = c("wide", "long"),
-                      add_coefs = FALSE) {
+                      add_coefs = FALSE, conf.int = FALSE) {
   
   format <- match.arg(format)
   if(add_coefs & format == "wide") stop("'add_coefs' only for format = 'long'")
@@ -71,7 +71,7 @@ dec_covar <- function(object, var_main, format = c("wide", "long"),
   var_all <- attr(terms(object), "term.labels")
   var_other <- var_all[! var_all %in% var_main]
   
-  ## base regression: on main variable(s) only
+  ## beta_main: base regression: on main variable(s) only
   formu_base <- drop_terms(f=formu_obj, dr=var_other)
   reg_base <- update(object, as.formula(formu_base))
   coef_diffs <- tidy(reg_base) %>%
@@ -81,7 +81,7 @@ dec_covar <- function(object, var_main, format = c("wide", "long"),
     select(term, diff)
   
   
-  ## auxiliary regs: covariates on main regs
+  ## gamma: auxiliary regs: covariates on main regs
   if(inherits(object, c("lm", "felm"))) {
     if(inherits(object, "lm")) {
       string_formula <- sprintf("cbind(%s) ~ %s", toString(var_other), paste(var_main, collapse=" + "))
@@ -89,23 +89,40 @@ dec_covar <- function(object, var_main, format = c("wide", "long"),
       string_formula <- sprintf("%s ~ %s", paste(var_other, collapse=" + "), paste(var_main, collapse=" + "))  
     }
     reg_aux <- update(object, as.formula(string_formula))  
-    gamma_df <- coef(reg_aux)[var_main,, drop=FALSE] %>%
-      as_data_frame() %>%
-      mutate(variable=var_main) %>%
-      gather(covariate, gamma, -variable)
+    gamma_df <- tidy(x=reg_aux, conf.int = conf.int) %>% 
+      select(response, term, estimate, contains("conf")) %>% 
+      filter(term %in% var_main) %>% 
+      rename(variable = term, 
+             covariate = response,
+             gamma = estimate) %>% 
+      rename_at(vars(contains("conf")), funs(str_replace(., "^conf\\.", "gamma_"))) %>% 
+      select(variable, covariate, contains("gamma"))
+    
+    
+    # gamma_df <- coef(reg_aux)[var_main,, drop=FALSE] %>%
+    #   as_data_frame() %>%
+    #   mutate(variable=var_main) %>%
+    #   gather(covariate, gamma, -variable)
+    
   } else {
     string_formula2 <- sprintf("%s ~ %s", var_other, paste(var_main, collapse=" + "))
     reg_aux_all <- lapply(string_formula2, function(x) update(object, as.formula(x))) 
-    gamma_df <- map2_dfr(reg_aux_all, var_other, ~tidy(.x) %>% mutate(covariate = .y)) %>%
+    gamma_df <- map2_dfr(reg_aux_all, var_other, ~tidy(.x, conf.int = conf.int) %>%
+                           mutate(covariate = .y)) %>%
       filter(term %in% var_main) %>%
       rename(variable = term,
              gamma= estimate) %>%
-      select(covariate, variable, gamma)
+      rename_at(vars(contains("conf")), funs(str_replace(., "^conf\\.", "gamma_"))) %>% 
+      select(covariate, variable, gamma, contains("gamma"))
   }
   
   ## Assemble results
-  res_df <- data_frame(covariate  = var_other,
-                       beta_K = coef(object)[var_other]) %>%
+  res_df <- tidy(object, conf.int = conf.int) %>% 
+    select(term, estimate, contains("conf")) %>% 
+    rename(covariate = term,
+           beta_K = estimate) %>% 
+    rename_at(vars(contains("conf")), funs(str_replace(., "^conf\\.", "beta_K_"))) %>% 
+    filter(covariate %in% var_other) %>%
     left_join(gamma_df, by="covariate") %>%
     mutate(delta = gamma * beta_K)
   
@@ -162,27 +179,95 @@ plot_dec <- function(x) {
   pl
 }
 
+plot_gamma_beta <-  function(x, colour = covariate, size = abs(delta), legend_size = FALSE,
+                             add_CI = FALSE) {
+  
+  colour_var <-  enquo(colour)
+  size_var <-  enquo(size)
+  
+  ## make sure data is in long?
+  if(!"gamma" %in% colnames(x)) stop("Data not in 'long' format?")
+  
+  res <- x %>% 
+    ggplot(aes(x = beta_K, y = gamma, colour = !!colour_var))+
+    geom_point(aes(size = !!size_var)) +
+    geom_hline(yintercept = 0, lty=2) +
+    geom_vline(xintercept = 0, lty=2) 
+  
+  if(!legend_size) res <- res +
+    scale_size_continuous(guide = "none")
+  
+  if(add_CI) res <-  res +
+    geom_errorbar(aes(ymin = gamma_low, ymax = gamma_high)) +
+    geom_errorbarh(aes(xmin = beta_K_low, xmax = beta_K_high)) 
+  res
+}
+
+
+## should be removed in near future, see: https://github.com/tidymodels/broom/issues/510
+process_lm_mine <- function(ret, x, conf.int = FALSE, conf.level = .95,
+                       exponentiate = FALSE) {
+  if (exponentiate) {
+    # save transformation function for use on confidence interval
+    if (is.null(x$family) ||
+        (x$family$link != "logit" && x$family$link != "log")) {
+      warning(paste(
+        "Exponentiating coefficients, but model did not use",
+        "a log or logit link function."
+      ))
+    }
+    trans <- exp
+  } else {
+    trans <- identity
+  }
+  
+  if (conf.int) {
+    # avoid "Waiting for profiling to be done..." message
+    CI <- suppressMessages(stats::confint(x, level = conf.level))
+    # Handle case if regression is rank deficient
+    p <- x$rank
+    if (!is.null(p) && !is.null(x$qr) & !inherits(x, "mlm")) {
+      piv <- x$qr$pivot[seq_len(p)]
+      CI <- CI[piv, , drop = FALSE]
+    }
+    colnames(CI) <- c("conf.low", "conf.high")
+    ret <- cbind(ret, trans(unrowname(CI)))
+  }
+  ret$estimate <- trans(ret$estimate)
+  
+  as_tibble(ret)
+}
+
+assignInNamespace("process_lm", process_lm_mine, "broom")
 
 #'###############################
 #'## Test
 ################################
 
 if(FALSE){
+  library(devtools)
+  devtools::install_github("tidymodels/broom")
   
   ## lm
   model_full_1 <- lm(y ~ lag.quarterly.revenue + price.index + income.level + market.potential, data=freeny)
   dec_covar(object = model_full_1, var_main = "lag.quarterly.revenue")
-  dec_lm1_l <- dec_covar(object = model_full_1, var_main = "lag.quarterly.revenue", format="long", add_coefs=TRUE)
+  dec_lm1_l <- dec_covar(object = model_full_1, var_main = "lag.quarterly.revenue", format="long", add_coefs=TRUE,
+                         conf.int = TRUE)
+  dec_lm1_l
   plot_dec(dec_lm1_l)
+  plot_gamma_beta(x=dec_lm1_l)
+  
   dec_lm1_k2_l <- dec_covar(object = model_full_1, var_main = c("lag.quarterly.revenue", "price.index"),
                             format="long", add_coefs=TRUE)
   plot_dec(dec_lm1_k2_l)
+  plot_gamma_beta(dec_lm1_k2_l)
   
   
   model_full_2 <- lm(Fertility ~ . , data=swiss)
   dec_covar(object = model_full_2, var_main = "Catholic")
   dec_lm2_l <- dec_covar(object = model_full_2, var_main = "Catholic", format="long", add_coefs=TRUE)
   plot_dec(dec_lm2_l)
+  plot_gamma_beta(dec_lm2_l)
 
   ## panel
   library(plm)
@@ -193,6 +278,7 @@ if(FALSE){
   dec_covar(object=zz, var_main = "log(pc)")
   dec_plm_l <- dec_covar(object=zz, var_main = "log(pc)", format="long", add_coefs=TRUE)
   plot_dec(dec_plm_l)
+  plot_gamma_beta(dec_plm_l)
   
   library(lfe)
   model_felm <- felm(log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp |state|0|state, data = Produc)
@@ -202,6 +288,7 @@ if(FALSE){
 
   dec_lfe_k2_l <- dec_covar(object=model_felm, var_main = c("log(pc)","log(pcap)"), format="long", add_coefs=TRUE)
   plot_dec(dec_lfe_k2_l)
+  plot_gamma_beta(dec_lfe_k2_l)
 
   ## iv
   library(AER)
@@ -212,8 +299,8 @@ if(FALSE){
   
   model_iv_just <- ivreg(log(packs) ~ log(rprice) + log(rincome) | log(rincome) + tdiff ,
                          data = CigarettesSW, subset = year == "1995")
-  dec_covar(object=model_iv_just, var_main = "log(rprice)")
-  
+  dec_iv <- dec_covar(object=model_iv_just, var_main = "log(rprice)")
+  plot_dec(dec_iv)
   
   ## in map calls
   df <- data_frame(data = list(freeny)) %>%
