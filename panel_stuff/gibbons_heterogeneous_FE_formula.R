356b2ec10108ebdc3cfe63b0e3f09310e387b292
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2019-10-26 16:49:39 -0700

    Update for case with two regressors, frish lauwell works

--- panel_stuff/gibbons_heterogeneous_FE_formula.R
+++ panel_stuff/gibbons_heterogeneous_FE_formula.R
@@ -33,7 +33,7 @@ reg_plm_bal <- plm(gsp ~ pcap, data = Produc, index = c("state","year"))
 regs_indiv <- Produc_wit_bal %>% 
   group_by(state) %>% 
   group_modify(~bind_cols(broom::tidy(lm(gsp ~ pcap-1, data=.x), quick=TRUE),
-                          summarise_at(.x, c("gsp", "pcap"), list(var=var)))) %>% 
+                          summarise_at(.x, c("gsp", "pcap", "pc"), list(var=var)))) %>% 
   ungroup()
 
 regs_indiv
@@ -45,3 +45,34 @@ regs_indiv
 ## variances
 with(regs_indiv, weighted.mean(estimate, pcap_var))
 coef(reg_plm_bal)
+
+
+################################
+#'## More than one regressor
+################################
+
+## global
+reg_plm_2reg <- plm(gsp ~ pcap + pc, data = Produc, index = c("state","year"))
+
+## frish waugh
+data_fw <- Produc_wit_bal %>% 
+  mutate(pcap_res = residuals(lm(pcap~pc, data=.)),
+         gsp_res = residuals(lm(gsp~pc, data=.)))
+
+reg_plw_fw_2reg <- lm(gsp_res~ pcap_res-1, data = data_fw)
+all.equal(coef(reg_plm_2reg)[1], coef(reg_plw_fw_2reg), check.attributes = FALSE)
+
+
+##
+regs_indiv_2reg <- data_fw %>% 
+  group_by(state) %>% 
+  group_modify(~bind_cols(broom::tidy(lm(gsp_res~ pcap_res-1, data=.x), quick=TRUE) %>% 
+                            spread(term, estimate),
+                          summarise_at(.x, c("gsp", "pcap", "pcap_res"), list(var=var)))) %>% 
+
+  ungroup()
+
+coef_hetero_2reg <- with(regs_indiv_2reg, weighted.mean(pcap_res, pcap_res_var))
+
+all.equal(coef(reg_plm_2reg)[1], coef_hetero_2reg, check.attributes = FALSE)
+
